88d604caea58534d76b555a224bb77b1bf98d150
Mustafa Öztürk <58623025+MstfOztrk@users.noreply.github.com>
2025-07-19 18:08:01 +0300

    Add GameConfig ScriptableObject and bind via installer

--- Assets/GameAssets/Scripts/Installers/ProjectContextInstaller.cs
+++ Assets/GameAssets/Scripts/Installers/ProjectContextInstaller.cs
@@ -3,10 +3,12 @@ using Zenject;
 public class ProjectContextInstaller : MonoInstaller
 {
     [SerializeField] private PoolManager poolManagerPrefab;
+    [SerializeField] private GameConfig gameConfig;
 
     public override void InstallBindings()
     {
         var poolManager = Container.InstantiatePrefabForComponent<PoolManager>(poolManagerPrefab);
         Container.Bind<IPoolManager>().FromInstance(poolManager).AsSingle().NonLazy();
+        Container.Bind<GameConfig>().FromInstance(gameConfig).AsSingle();
     }
 }
