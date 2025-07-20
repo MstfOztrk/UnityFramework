089fd0611203a0f670ec18200c7d2eea34a1d694
Mustafa Öztürk <oztrk.mstf.43@gmail.com>
2025-07-20 19:09:22 +0300

    Folder Arrangment

--- Assets/GameAssets/Scripts/Systems/GameManager.cs
+++ Assets/GameAssets/Scripts/Systems/GameManager.cs
@@ -5,18 +5,25 @@ using Zenject;
 public class GameManager : MonoBehaviour
 {
     [Inject] private IEventBus eventBus;
-
+    private bool isGameStarted = false;
     private void OnEnable()
     {
         LeanTouch.OnFingerTap += HandleFingerTap;
     }
+    
+    private void OnDisable()
+    {
+        LeanTouch.OnFingerTap -= HandleFingerTap;
+    }
     private void HandleFingerTap(LeanFinger finger)
     {
+        if (isGameStarted) return;
         if (finger.IsOverGui == false)
         {
             print("game start");
+            isGameStarted = true;
             eventBus.Raise(GameEvent.GameStart);
-            LeanTouch.OnFingerTap -= HandleFingerTap;
+
         }
     }
 }
